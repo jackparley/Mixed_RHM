96264fd6761e5d18da6497c2c2217ad3b9d4681c
Jack Thomas Parley <parley@pcslsrv2.epfl.ch>
2025-03-06 12:38:06 +0100

    New model

--- models/__init__.py
+++ models/__init__.py
@@ -3,5 +3,6 @@ from .fcn import Perceptron, MLP
 from .cnn import hCNN
 from .cnn import hCNN_mixed
 from .cnn import hCNN_inside
+from .cnn import hCNN_Gen
 from .lcn import hLCN
 from .transformer import MultiHeadAttention, MLA
