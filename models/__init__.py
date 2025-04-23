e9e16dbf4118d6891e47d78c0ff799260c603a32
Jack Thomas Parley <parley@kuma2.kuma.cluster>
2025-04-23 16:44:29 +0200

    New models

--- models/__init__.py
+++ models/__init__.py
@@ -4,6 +4,8 @@ from .cnn import hCNN
 from .cnn import hCNN_mixed
 from .cnn import hCNN_inside
 from .cnn import hCNN_inside_L_2
+from .cnn import hCNN_sharing
+from .cnn import hCNN_no_sharing
 from .cnn import hCNN_Gen
 from .lcn import hLCN
 from .transformer import MultiHeadAttention, MLA
