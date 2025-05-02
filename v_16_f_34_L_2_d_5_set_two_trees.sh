5e8aa89ade9f8f132fc272cb5a57c033d3528db3
Jack Thomas Parley <parley@kuma1.kuma.cluster>
2025-05-02 11:57:11 +0200

    New sh file

---
+++ v_16_f_34_L_2_d_5_set_two_trees.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+
+#SBATCH --job-name mixed_rhm_v_16_f_34_L_2_T1_double_sharing_65536  # Assign unique name
+#SBATCH --chdir /scratch/parley/   # go to scratch
+#SBATCH -o /home/parley/running/%A.%x_%a.out # STDOUT
+#SBATCH -e /home/parley/running/%A.%x_%a.err # STDERR
+
+#SBATCH --partition h100
+#SBATCH --time 08:00:00
+#SBATCH --mem 90G
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=4
+#SBATCH --gres gpu:1
+#SBATCH --account pcsl
+#SBATCH --array=0-9%10
+
+
+# Define dataset arguments
+
+insert_train=$1
+
+seed1=$(od -An -N3 -i /dev/random)
+#seed1=100
+#seed2=100
+seed2=$(od -An -N3 -i /dev/random)
+seed3=$(od -An -N3 -i /dev/random)
+#seed3=100
+
+
+DATASET='mixed_rhm'
+NUM_FEATURES=16
+NUM_CLASSES=16
+FRACTION_RULES=0.75
+RULE_SEQUENCE_TYPE=1
+NUM_LAYERS=2
+NUM_TOKENS=5
+#SEED_RULES=seed1
+#TRAIN_SIZE=8064
+BATCH_SIZE=128
+MAX_DATA=884736
+#TEST_SIZE=768
+#SEED_SAMPLE=seed2
+INPUT_FORMAT="onehot"
+WHITENING=1
+NON_OVERLAPPING=0
+D_5_SET=1
+
+# Define architecture arguments
+MODEL="hcnn_sharing"
+DEPTH=2
+WIDTH=65536
+WIDTH_2=256
+#BIAS_FLAG="--bias"  # Use "--bias" to enable, remove it if not needed
+#SEED_MODEL=seed3
+
+# Define training arguments
+LEARNING_RATE=1.0
+OPTIMIZER="sgd"
+ACCUMULATION=1
+MOMENTUM=0.9
+#SCHEDULER=None
+SCHEDULER_TIME=1024
+MAX_EPOCHS=2048
+
+# Define output arguments
+PRINT_FREQ=64
+SAVE_FREQ=3
+CHECKPOINTS_FLAG="--checkpoints"  # Use "--checkpoints" to enable, remove it if not needed
+LOSS_THRESHOLD=0.001
+OUTNAME="dynamics_v_16_f_34_L_2_d_5_set_P_${insert_train}_${SLURM_ARRAY_TASK_ID}.pkl"
+
+
+path="${SLURM_JOB_NAME}" # create directory with ID
+mkdir -p $path
+cd $path
+
+echo STARTING AT
+date
+
+
+# Run the experiment with all arguments
+srun python /home/parley/Mixed_RHM/main_mixed.py \
+    --device cuda \
+    --dataset "$DATASET" \
+    --num_features "$NUM_FEATURES" \
+    --num_classes "$NUM_CLASSES" \
+    --num_tokens "$NUM_TOKENS" \
+    --fraction_rules "$FRACTION_RULES" \
+    --rule_sequence_type "$RULE_SEQUENCE_TYPE" \
+    --num_layers "$NUM_LAYERS" \
+    --seed_rules ${seed1} \
+    --train_size ${insert_train} \
+    --max_data "$MAX_DATA" \
+    --batch_size "$BATCH_SIZE" \
+    --seed_sample ${seed2} \
+    --input_format "$INPUT_FORMAT" \
+    --whitening "$WHITENING" \
+    --non_overlapping "$NON_OVERLAPPING" \
+    --d_5_set "$D_5_SET" \
+    --model "$MODEL" \
+    --depth "$DEPTH" \
+    --width "$WIDTH" \
+    --width_2 "$WIDTH_2" \
+    $BIAS_FLAG \
+    $CHECKPOINTS_FLAG \
+    --seed_model ${seed3} \
+    --lr "$LEARNING_RATE" \
+    --optim "$OPTIMIZER" \
+    --accumulation "$ACCUMULATION" \
+    --momentum "$MOMENTUM" \
+    --scheduler_time "$SCHEDULER_TIME" \
+    --max_epochs "$MAX_EPOCHS" \
+    --print_freq "$PRINT_FREQ" \
+    --save_freq "$SAVE_FREQ" \
+    --loss_threshold "$LOSS_THRESHOLD" \
+    --outname "$OUTNAME"
+
+
+
+echo FINISHED AT
+date
