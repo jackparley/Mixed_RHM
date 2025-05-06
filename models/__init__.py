9c84924aacfca935b01380b4678f0b77ec2afe8b
Jack Thomas Parley <parley@kuma2.kuma.cluster>
2025-05-06 12:32:45 +0200

    New models

--- models/__init__.py
+++ models/__init__.py
@@ -2,8 +2,9 @@ import torch
 from .fcn import Perceptron, MLP
 from .cnn import hCNN
 from .cnn import hCNN_mixed
-from .cnn import hCNN_inside
 from .cnn import hCNN_inside_L_2
+from .cnn import hCNN_inside_L_3
+from .cnn import hCNN_inside_L_4
 from .cnn import hCNN_sharing
 from .cnn import hCNN_no_sharing
 from .cnn import hCNN_Gen
