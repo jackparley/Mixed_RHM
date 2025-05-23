8aa2fe2d5e502e0f380e95804bd501c28f41d4d5
Jack Thomas Parley <parley@pcslsrv2.epfl.ch>
2025-05-23 14:13:02 +0200

    mlp dim

--- models/__init__.py
+++ models/__init__.py
@@ -8,5 +8,6 @@ from .cnn import hCNN_inside_L_4
 from .cnn import hCNN_sharing
 from .cnn import hCNN_no_sharing
 from .cnn import hCNN_Gen
+from .cnn import hCNN_Gen_MLP
 from .lcn import hLCN
 from .transformer import MultiHeadAttention, MLA
