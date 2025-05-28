ad8db99fdee80c1038ac5745eec45ee0e391bdd8
Jack Thomas Parley <parley@kuma2.kuma.cluster>
2025-05-28 12:36:19 +0200

    No sharing model

--- models/__init__.py
+++ models/__init__.py
@@ -7,6 +7,7 @@ from .cnn import hCNN_inside_L_3
 from .cnn import hCNN_inside_L_4
 from .cnn import hCNN_sharing
 from .cnn import hCNN_no_sharing
+from .cnn import hCNN_no_sharing_Gen
 from .cnn import hCNN_Gen
 from .cnn import hCNN_Gen_MLP
 from .lcn import hLCN
