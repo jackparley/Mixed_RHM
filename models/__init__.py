8f1f975c1a63e0e5bf859bc62a98ca28df321952
Jack Thomas Parley <parley@pcslsrv1.epfl.ch>
2025-06-23 14:19:26 +0200

    Treebank

--- models/__init__.py
+++ models/__init__.py
@@ -3,6 +3,7 @@ from .fcn import Perceptron, MLP
 from .cnn import hCNN
 from .cnn import hCNN_mixed
 from .cnn import hCNN_inside_L_2
+from .cnn import hCNN_inside_L_2_tree_topologies
 from .cnn import hCNN_inside_L_3
 from .cnn import hCNN_inside_L_4
 from .cnn import hCNN_sharing
